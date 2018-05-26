1fad1044b1729cc3677ca4d69942f269e64e99a1
schmargi <maike@schmargi.de>
2018-05-27 00:26:26 +0200

    🤷🏻‍♀️

--- frontend/app/controllers/tags/index.js
+++ frontend/app/controllers/tags/index.js
@@ -2,17 +2,16 @@ import Controller from '@ember/controller';
 // import {filterBy} from '@ember/object/computed';
 
 export default Controller.extend({
-  queryParams: ['city'],
+  queryParams: ['location'],
 
   // filteredItems: filterBy('model.posts','location', 'Regensburg'),
 
   actions: {
-
     locationSelection(locale) {
       var buttonWithId = document.getElementById("location");
       buttonWithId.innerHTML = locale;
-      this.set('city', locale);
-      this.transitionTo( 'tags',{ queryParams: { city: locale}});
+      this.set('location', locale);
+      // this.transitionTo( 'tags',{ queryParams: { location: locale}});
     }
 
   }
