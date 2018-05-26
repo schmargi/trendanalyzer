c3a9ae87801bf162de4299b23c5dc166defbc1b6
schmargi <maike@schmargi.de>
2018-05-26 22:36:40 +0200

    🤯

--- frontend/app/models/tag.js
+++ frontend/app/models/tag.js
@@ -1,13 +1,35 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default DS.Model.extend({
   title:           DS.attr('string'),
   posts:           DS.hasMany('post', {async: true}),
 
-  today:           '26. Mai',
-  yesterday:       '25. Mai',
-  yesteryesterday: '24. Mai',
+  today:                      moment().format('YYYY-MM-DD'),
+  yesterday:                  moment().subtract(1,'days').format('YYYY-MM-DD'),
+  presterday:                 moment().subtract(2,'days').format('YYYY-MM-DD'),
+  one_before_presterday:      moment().subtract(3,'days').format('YYYY-MM-DD'),
+  two_before_presterday:      moment().subtract(4,'days').format('YYYY-MM-DD'),
+
+  today_like_count:     DS.attr('number'),
+  today_retweet_count:  DS.attr('number'),
+  today_post_count:     DS.attr('number'),
+
+  yesterday_like_count:     DS.attr('number'),
+  yesterday_retweet_count:  DS.attr('number'),
+  yesterday_post_count:     DS.attr('number'),
+
+  presterday_like_count:     DS.attr('number'),
+  presterday_retweet_count:  DS.attr('number'),
+  presterday_post_count:     DS.attr('number'),
+
+  one_before_presterday_like_count:     DS.attr('number'),
+  one_before_presterday_retweet_count:  DS.attr('number'),
+  one_before_presterday_post_count:     DS.attr('number'),
+  two_before_presterday_like_count:     DS.attr('number'),
+  two_before_presterday_retweet_count:  DS.attr('number'),
+  two_before_presterday_post_count:     DS.attr('number'),
 
   is_favorite:     DS.attr('boolean', { defaultValue: false }),
   post_count:      DS.attr('number'),
@@ -18,30 +40,34 @@ export default DS.Model.extend({
   contains_instagram: DS.attr('boolean'),
 
 
-
-
   data_post_count:  computed('post_count', function(){
     return [
       ['Days', 'Post Count'],
-      [this.get('today'), this.get('post_count')],
-      [this.get('yesterday'), this.get('post_count')],
-      [this.get('yesteryesterday'), this.get('post_count')],
+      [this.get('one_before_presterday'), this.get('one_before_presterday_post_count')],
+      [this.get('two_before_presterday'), this.get('two_before_presterday_post_count')],
+      [this.get('presterday'), this.get('presterday_post_count')],
+      [this.get('yesterday'), this.get('yesterday_post_count')],
+      [this.get('today'), this.get('today_post_count')],
     ];
   }),
   data_like_count:  computed('like_count', function(){
     return [
       ['Days', 'Like Count'],
-      [this.get('today'), this.get('like_count')],
-      [this.get('yesterday'), this.get('like_count')],
-      [this.get('yesteryesterday'), this.get('like_count')],
+      [this.get('one_before_presterday'), this.get('one_before_presterday_like_count')],
+      [this.get('two_before_presterday'), this.get('two_before_presterday_like_count')],
+      [this.get('presterday'), this.get('presterday_like_count')],
+      [this.get('yesterday'), this.get('yesterday_like_count')],
+      [this.get('today'), this.get('today_like_count')],
     ];
   }),
   data_retweet_count:  computed('retweet_count', function(){
     return [
       ['Days', 'Retweet Count'],
-      [this.get('today'), this.get('retweet_count')],
-      [this.get('yesterday'), this.get('retweet_count')],
-      [this.get('yesteryesterday'), this.get('retweet_count')],
+      [this.get('one_before_presterday'), this.get('one_before_presterday_retweet_count')],
+      [this.get('two_before_presterday'), this.get('two_before_presterday_retweet_count')],
+      [this.get('presterday'), this.get('presterday_retweet_count')],
+      [this.get('yesterday'), this.get('yesterday_retweet_count')],
+      [this.get('today'), this.get('today_retweet_count')],
     ];
   }),
 
