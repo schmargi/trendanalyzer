3e377612ec03f3f99966738c91fb04b90e8b5218
schmargi <maike@schmargi.de>
2018-05-26 13:19:42 +0200

    wuhuuuuu

--- frontend/app/models/tag.js
+++ frontend/app/models/tag.js
@@ -1,7 +1,28 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
   title:           DS.attr('string'),
   posts:           DS.hasMany('post', {async: true}),
   is_favorite:     DS.attr('boolean', { defaultValue: false }),
+
+  mydata:          [
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ],
+
+  myoptions:      {
+      title: 'How I spend my days',
+      height: 300,
+      width: 400,
+
+      animation: {
+        startup: true,
+        easing: 'inAndOut',
+      },
+    },
 });
