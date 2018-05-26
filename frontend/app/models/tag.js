d2a280c27ed5cd883ae2be2fe331ce5ca06ad3f6
schmargi <maike@schmargi.de>
2018-05-26 20:14:44 +0200

    new charts

--- frontend/app/models/tag.js
+++ frontend/app/models/tag.js
@@ -5,13 +5,18 @@ export default DS.Model.extend({
   title:           DS.attr('string'),
   posts:           DS.hasMany('post', {async: true}),
 
+  today:           '26. Mai',
+  yesterday:       '25. Mai',
+  yesteryesterday: '24. Mai',
+
+  
+
   postCount:      computed.readOnly('posts.length'),
   is_favorite:     DS.attr('boolean', { defaultValue: false }),
 
   likeCountSum:   computed('posts.like_count', function(){
     let posts = this.get('posts').mapBy('like_count');
     var sum = 0;
-    console.log(posts);
     for (var i = 0; i < posts.length; i++) {
       sum += posts[i]
     }
@@ -37,21 +42,78 @@ export default DS.Model.extend({
     return posts.indexOf("INSTAGRAM") > -1;
   }),
 
-  mydata:        computed('postCount', function(){
+  data_post_count:  computed('postCount', function(){
     return [
       ['Days', 'Post Count'],
-      ['23.Mai', this.get('postCount')],
-      ['two days ago', this.get('postCount')],
-      ['Yesterday', this.get('postCount')],
-      ['26. Mai', this.get('postCount')],
+      [this.get('today'), this.get('postCount')],
+      [this.get('yesterday'), this.get('postCount')],
+      [this.get('yesteryesterday'), this.get('postCount')],
+    ];
+  }),
+  data_like_count:  computed('like_count', function(){
+    return [
+      ['Days', 'Like Count'],
+      [this.get('today'), this.get('likeCountSum')],
+      [this.get('yesterday'), this.get('likeCountSum')],
+      [this.get('yesteryesterday'), this.get('likeCountSum')],
+    ];
+  }),
+  data_retweet_count:  computed('retweet_count', function(){
+    return [
+      ['Days', 'Retweet Count'],
+      [this.get('today'), this.get('retweetCountSum')],
+      [this.get('yesterday'), this.get('retweetCountSum')],
+      [this.get('yesteryesterday'), this.get('retweetCountSum')],
     ];
   }),
 
-  myoptions:       computed('title', function() {
+  retweet_options: computed(function() {
     return {
-        title: this.get('title'),
+        title: 'Retweet Count',
         height: 300,
-        width: 400,
+        legend: {position: 'none'},
+        vAxis: {
+        viewWindow: {
+            min:0
+          }
+        },
+        colors: 'blue',
+
+        animation: {
+          startup: true,
+          easing: 'inAndOut',
+        },
+      };
+    }),
+  like_options: computed(function() {
+    return {
+        title: 'Like Count',
+        height: 300,
+        legend: {position: 'none'},
+        vAxis: {
+        viewWindow: {
+            min:0
+          }
+        },
+        colors: 'green',
+
+        animation: {
+          startup: true,
+          easing: 'inAndOut',
+        },
+      };
+    }),
+  post_options: computed(function() {
+    return {
+        title: 'Post Count',
+        height: 300,
+        legend: {position: 'none'},
+        vAxis: {
+        viewWindow: {
+            min:0
+          }
+        },
+        colors: 'red',
 
         animation: {
           startup: true,
