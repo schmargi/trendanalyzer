024a91a0fffeef698c8821dcd8edc79e0122a90e
schmargi <maike@schmargi.de>
2018-05-26 15:29:18 +0200

    add infos to tags route

--- frontend/app/models/tag.js
+++ frontend/app/models/tag.js
@@ -4,25 +4,59 @@ import { computed } from '@ember/object';
 export default DS.Model.extend({
   title:           DS.attr('string'),
   posts:           DS.hasMany('post', {async: true}),
+
+  postCount:       Ember.computed.readOnly('posts.length'),
   is_favorite:     DS.attr('boolean', { defaultValue: false }),
 
-  mydata:          [
-      ['Task', 'Hours per Day'],
-      ['Work', 11],
-      ['Eat', 2],
-      ['Commute', 2],
-      ['Watch TV', 2],
-      ['Sleep', 7],
-    ],
+  likeCountSum:    Ember.computed('posts.like_count', function(){
+    let posts = this.get('posts').mapBy('like_count');
+    var sum = 0;
+    console.log(posts);
+    for (var i = 0; i < posts.length; i++) {
+      sum += posts[i]
+    }
+    return sum;
+  }),
 
-  myoptions:      {
-      title: 'How I spend my days',
-      height: 300,
-      width: 400,
+  retweetCountSum:    Ember.computed('posts.retweet_count', function(){
+    let posts = this.get('posts').mapBy('retweet_count');
+    var sum = 0;
+    for (var i = 0; i < posts.length; i++) {
+      sum += posts[i]
+    }
+    return sum;
+  }),
 
-      animation: {
-        startup: true,
-        easing: 'inAndOut',
-      },
-    },
+  hasTwitter:      Ember.computed('posts.origin', function(){
+    let posts = this.get('posts').mapBy('origin');
+    return posts.indexOf("TWITTER") > -1;
+  }),
+
+  hasInstagram:      Ember.computed('posts.origin', function(){
+    let posts = this.get('posts').mapBy('origin');
+    return posts.indexOf("INSTAGRAM") > -1;
+  }),
+
+  mydata:        computed('postCount', function(){
+    return [
+      ['Days', 'Post Count'],
+      ['23.Mai', this.get('postCount')],
+      ['two days ago', this.get('postCount')],
+      ['Yesterday', this.get('postCount')],
+      ['26. Mai', this.get('postCount')],
+    ];
+  }),
+
+  myoptions:       computed('title', function() {
+    return {
+        title: this.get('title'),
+        height: 300,
+        width: 400,
+
+        animation: {
+          startup: true,
+          easing: 'inAndOut',
+        },
+      };
+    }),
 });
